89be6eecf4c89e337c6fe9fe95b439a6f384bb78
Teng-Fong Lee <cs15lsp23cn@ieng6-202.ucsd.edu>
2023-05-22 19:30:23 -0700

    change index1 to index2

--- ListExamples.java
+++ ListExamples.java
@@ -40,7 +40,7 @@ class ListExamples {
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
-      // change index1 below to index2 to fix test
+      // change index1 below to index2 to fix test if possible
       index2 += 1;
     }
     return result;
